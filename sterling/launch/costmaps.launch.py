ea14b1193e58d9b87391db66296bd329293255ab
Nick Chan <nick.chan@utexas.edu>
2025-03-27 13:06:47 -0500

    RViz added

--- sterling/launch/costmaps.launch.py
+++ sterling/launch/costmaps.launch.py
@@ -1,15 +1,56 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+)
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     params_file = os.path.join(get_package_share_directory("sterling"), "config", "params.yaml")
-    namespace = "panther/sterling" # Namespaced so topics have namespace/node_name or namespace/topic_name
+    sterling_dir = get_package_share_directory("sterling")
+
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_rviz = LaunchConfiguration("use_rviz")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
+
+    declare_namespace_arg = DeclareLaunchArgument(
+        "namespace",
+        default_value="panther/sterling",
+        description="Add namespace to all launched nodes.",
+    )
+    declare_use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true.",
+    )
+    declare_use_rviz_arg = DeclareLaunchArgument(
+        "use_rviz",
+        default_value="true",
+        description="Whether to start RViz2.",
+        choices=["true", "false"],
+    )
+    declare_rviz_config_file_arg = DeclareLaunchArgument(
+        "rviz_config_file",
+        default_value=os.path.join(sterling_dir, "config", "panther_sim.rviz"),
+        description="Full path to the RVIZ config file to use",
+    )
 
     return LaunchDescription(
         [
+            declare_namespace_arg,
+            declare_use_sim_time_arg,
+            declare_use_rviz_arg,
+            declare_rviz_config_file_arg,
             Node(
                 package="sterling",
                 executable="local_costmap_builder",
@@ -23,6 +64,18 @@ def generate_launch_description():
                 name="global_costmap_builder",
                 namespace=namespace,
                 parameters=[params_file],
-            )
+            ),
+            Node(
+                condition=IfCondition(use_rviz),
+                package="rviz2",
+                executable="rviz2",
+                name="rviz_mapping",
+                arguments=[
+                    "-d",
+                    PathJoinSubstitution([rviz_config_file]),
+                ],
+                parameters=[{"use_sim_time": use_sim_time}],
+                output="screen",
+            ),
         ]
     )
