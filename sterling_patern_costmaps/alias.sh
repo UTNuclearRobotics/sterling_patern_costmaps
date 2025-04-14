c18a51460504c07b609c81803ceb2d211e8eca2e
Ryan Lee <ryanlee0518@utexas.edu>
2025-04-14 15:54:49 -0500

    commit in prep for marco to debug

--- sterling_patern_costmaps/alias.sh
+++ sterling_patern_costmaps/alias.sh
@@ -6,16 +6,16 @@ alias run_gazebo_high='roslaunch sterling_gazebo sidewalks.launch.py high_res:=t
 
 ### Step 2: Launch Nav2
 # Sterling parameters
-alias run_nav2='roslaunch utexas_panther bringup_launch.py namespace:=panther observation_topic:=ouster/scan observation_topic_type:=laserscan slam:=true use_rviz:=True use_sim_time:=true params_file:=/root/ros_ws/src/sterling_patern_costmaps/config/nav2_params.yaml'
+alias run_nav='roslaunch sterling_patern_costmaps costmaps.launch'
 
 # How to send a goal_pose now and the alias does not work
 function run_goal() {
     local x=${1:-0.0}
     local y=${2:-0.0}
-    ros2 topic pub /panther/goal_pose geometry_msgs/msg/PoseStamped "{ header: { frame_id: 'panther/map' }, pose: { position: { x: $x, y: $y }, orientation: { x: 0, y: 0, z: 0, w: 1 } } }"
+    rostopic pub /panther/sterling/move_base_simple/goal geometry_msgs/PoseStamped "header: { frame_id: 'panther/sterling/map' }, pose: { position: { x: $x, y: $y, z: 0.0 }, orientation: { x: 0.0, y: 0.0, z: 0.0, w: 1.0 } }" -1
 }
 
 ### Step 3: Sterling costmap node
-alias run_sterling_patern_costmap='source /root/ros_ws/install/setup.bash && ros2 launch sterling_patern_costmaps costmaps.launch.py'
+alias run_sterling_patern_costmap='source /root/ros_ws/devel/setup.bash && roslaunch sterling_patern_costmaps costmaps.launch'
 # While node is running, call service to save the costmap
-alias save_costmap='source /root/ros_ws/install/setup.bash && ros2 service call /sterling_patern_costmaps/save_costmap std_srvs/srv/Trigger'
+alias save_costmap='source /root/ros_ws/devel/setup.bash && rosservice call /sterling_patern_costmaps/save_costmap "{}"'
