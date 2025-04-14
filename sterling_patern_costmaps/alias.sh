a89eec40ebfd3c1f7b7c6a7c9c20cb396497605e
Ryan Lee <ryanlee0518@utexas.edu>
2025-04-14 14:08:53 -0500

    Changes to model path folder in params.yaml

--- sterling_patern_costmaps/alias.sh
+++ sterling_patern_costmaps/alias.sh
@@ -1,12 +1,12 @@
 # Commands to run inside the container
 
 ### Step 1: Choose the terrain resolution you want ot run the Gazebo world
-alias run_gazebo_low='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=false namespace:=panther'
-alias run_gazebo_high='ros2 launch sterling_gazebo sidewalks.launch.py high_res:=true namespace:=panther'
+alias run_gazebo_low='roslaunch sterling_gazebo sidewalks.launch.py high_res:=false namespace:=panther'
+alias run_gazebo_high='roslaunch sterling_gazebo sidewalks.launch.py high_res:=true namespace:=panther'
 
 ### Step 2: Launch Nav2
 # Sterling parameters
-alias run_nav2='ros2 launch utexas_panther bringup_launch.py namespace:=panther observation_topic:=ouster/scan observation_topic_type:=laserscan slam:=true use_rviz:=True use_sim_time:=true params_file:=/root/ros2_ws/src/sterling_patern_costmaps/config/nav2_params.yaml'
+alias run_nav2='roslaunch utexas_panther bringup_launch.py namespace:=panther observation_topic:=ouster/scan observation_topic_type:=laserscan slam:=true use_rviz:=True use_sim_time:=true params_file:=/root/ros_ws/src/sterling_patern_costmaps/config/nav2_params.yaml'
 
 # How to send a goal_pose now and the alias does not work
 function run_goal() {
@@ -16,6 +16,6 @@ function run_goal() {
 }
 
 ### Step 3: Sterling costmap node
-alias run_sterling_patern_costmap='source /root/ros2_ws/install/setup.bash && ros2 launch sterling_patern_costmaps costmaps.launch.py'
+alias run_sterling_patern_costmap='source /root/ros_ws/install/setup.bash && ros2 launch sterling_patern_costmaps costmaps.launch.py'
 # While node is running, call service to save the costmap
-alias save_costmap='source /root/ros2_ws/install/setup.bash && ros2 service call /sterling_patern_costmaps/save_costmap std_srvs/srv/Trigger'
+alias save_costmap='source /root/ros_ws/install/setup.bash && ros2 service call /sterling_patern_costmaps/save_costmap std_srvs/srv/Trigger'
