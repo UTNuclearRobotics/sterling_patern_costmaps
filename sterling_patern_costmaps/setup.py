19761256279ed5fad055dd208c3af0f48614d42a
Ryan Lee <ryanlee0518@utexas.edu>
2025-04-14 13:47:24 -0500

    Updates to package.xml, costmaps.launch file, setup.py, and rviz config file.

--- sterling_patern_costmaps/setup.py
+++ sterling_patern_costmaps/setup.py
@@ -11,9 +11,16 @@ setup(
     packages=find_packages(exclude=["test"]),
     data_files=[
         (os.path.join("share", package_name), ["package.xml"]),
-        (os.path.join("share", package_name, "config"), [os.path.join("config", "params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "move_base_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "costmap_common_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "global_costmap_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "local_costmap_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "slam_toolbox_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "robot_state_publisher_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "map_server_params.yaml")]),
+        (os.path.join("share", package_name, "config"), [os.path.join("config", "custom_nodes_params.yaml")]),
         (os.path.join("share", package_name, "config"), [os.path.join("config", "panther_sim.rviz")]),
-        (os.path.join("share", package_name, "launch"), [os.path.join("launch", "costmaps.launch.py")]),
+        (os.path.join("share", package_name, "launch"), [os.path.join("launch", "costmaps.launch")]),
         (os.path.join("lib", package_name), lib_files),
     ],
     install_requires=[
@@ -28,8 +35,8 @@ setup(
         "torchvision",
     ],
     zip_safe=True,
-    maintainer="nchan",
-    maintainer_email="nick.chan@utexas.edu",
+    maintainer="asharp",
+    maintainer_email="asharp@utexas.edu",
     description="Package for converting sterling/patern output into ROS costmaps",  # Updated to match package.xml
     license="TODO: License declaration",
     entry_points={
