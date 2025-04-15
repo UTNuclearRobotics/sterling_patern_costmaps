3597df967130b7ad876de7d0aec2317d9ae40de0
Marco Morales <marco.morales@austin.utexas.edu>
2025-04-15 09:01:02 -0500

    entry point script is included

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,22 +2,7 @@
 
 set -e
 
-# Adjust UID and GID if provided
-if [ -n "$UID" ] && [ -n "$GID" ]; then
-  # Check if 'user' group exists and modify it, or create a new group
-  groupmod -g "$GID" user 2>/dev/null || groupadd -g "$GID" user
-  # Check if 'user' exists and modify it, or create a new user
-  usermod -u "$UID" -g "$GID" user 2>/dev/null || useradd -m -u "$UID" -g "$GID" user
-  export HOME=/home/user
-  # Execute the command as the specified user
-  exec gosu user "$@"
-fi
-
-# Source ROS1 Noetic environment
 source /opt/ros/noetic/setup.bash
 
-# Source the workspace
-source /root/ros_ws/devel/setup.bash
-
-# Execute the command passed to the container
-exec "$@"
+# execute a command in the container
+exec /bin/bash
