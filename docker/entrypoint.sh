f256d3aacbda37a65bf7fe7b3a22e341bfac9bb6
ryanl0518 <ryanlee0518@utexas.edu>
2026-01-28 13:41:40 -0600

    workspace restructuring

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source /opt/ros/humble/setup.bash
-
-# execute a command in the container
-exec /bin/bash
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = "sterling_patern_costmaps"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        # Register the package with ament index and include package.xml, config files, and launch files
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", glob("config/*.yaml") + glob("config/*.rviz")),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py") + glob("launch/*.py")),
+    ],
+    install_requires=[
+        "setuptools",
+        "opencv-python",
+        "numpy",
+        "joblib",
+        "scikit-learn",
+        "torch",
+        "pyyaml",
+        "tensorflow",
+        "torchvision",
+    ],
+    zip_safe=True,
+    maintainer="rlee",
+    maintainer_email="ryanlee0518@utexas.edu",
+    description="Global and local costmap builder nodes for pattern-based exploration",
+    license="TODO",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "global_costmap_builder = sterling_patern_costmaps.nodes.global_costmap_builder:main",
+            "local_costmap_builder = sterling_patern_costmaps.nodes.local_costmap_builder:main",
+        ],
+    },
+)
